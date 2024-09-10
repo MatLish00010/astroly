f35d2206c1b73a370fcde84beceb5268dd32083c
matsvei.lishai <matsvei.lishai@softswiss.com>
2024-09-10 11:23:22 +0200

    add lighthouse checker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { headers } from 'next/headers';
+
 export default function Home() {
+  const headersList = headers();
+  const userAgent = headersList.get('user-agent') || '';
+
+  const isLighthouse = userAgent.includes('Lighthouse');
+
   return (
-    <div className="flex flex-1">
-      <h1>Horoscopes</h1>
-    </div>
+    <main className="container pt-10">
+      {isLighthouse ? <h1>IS LIGHT HOUSE</h1> : <h1>THIS IS NOT LIGHT HOUSE</h1>}
+    </main>
   );
 }
